43d97796df4002fa093ecf1294230b165660af82
jhastwell <jhastwell@solocal.com>
2019-06-25 13:53:13 +0200

    Change int formatting and add bottle dependency

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ from time import sleep
 
 app = Flask(__name__)
 
+
 @app.route('/', defaults={'name' : 'World'})
 @app.route('/<name>')
 def hello_world(name):
     return 'Hello {}!\n'.format(name)
+
 
 @app.route('/end/', defaults={'name': 'Earth'})
 @app.route('/end/<name>')
 def goodbye_earth(name):
     return 'Goodbye {}!\n'.format(name)
 
+
 @app.route('/wait/', defaults={'ms': 10})
 @app.route('/wait/<int:ms>')
 def wait(ms):
     sleep(ms / 1000.0)
-    return 'Waited {} milliseconds!\n'.format(str(ms))
+    return 'Waited {ms:d} milliseconds!\n'.format(ms=ms)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8801)))
